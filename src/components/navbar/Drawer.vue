2b5883d82406276c807954efaceb93833c4b7f49
tirthamoulib <btirthamouli@gmail.com>
2020-02-23 02:02:46 +0530

    Init

---
+++ src/components/navbar/Drawer.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-list>
+    <v-list-item v-for="link in links" :key="link.text" router :to="link.route">
+      <!-- Icon -->
+      <v-list-item-action>
+        <v-icon class="white--text">{{ link.icon }}</v-icon>
+      </v-list-item-action>
+
+      <!-- Text -->
+      <v-list-item-content>
+        <v-list-item-title class="white--text">
+          {{ link.text }}
+        </v-list-item-title>
+      </v-list-item-content>
+    </v-list-item>
+  </v-list>
+</template>
+
+<script>
+export default {
+  name: "Drawer",
+  data() {
+    return {
+      /**
+       * All the links
+       */
+      links: [
+        { icon: "dashboard", text: "Dashboard", route: "/" },
+        { icon: "person", text: "Add User", route: "/add/user" },
+        { icon: "movie", text: "Add Movie", route: "/add/movie" }
+      ]
+    };
+  }
+};
+</script>
---
+++ src/components/navbar/Navbar.vue
@@ -0,0 +1,52 @@
+<template>
+  <nav>
+    <v-app-bar flat app>
+      <!-- Hamburger icon -->
+      <v-app-bar-nav-icon class="grey--text" @click="toggleShowDrawer" />
+
+      <!-- Title -->
+      <v-toolbar-title class="text-uppercase grey--text">
+        <span class="font-weight-light">PVR</span>
+        <span>Cinema</span>
+      </v-toolbar-title>
+
+      <!-- Spacer -->
+      <v-spacer />
+
+      <!-- Signout button -->
+      <v-btn text color="grey">
+        <span>Sign out</span>
+        <v-icon right>exit_to_app</v-icon>
+      </v-btn>
+    </v-app-bar>
+
+    <!-- Navigation drawer -->
+    <v-navigation-drawer v-model="showDrawer" app class="indigo">
+      <drawer />
+    </v-navigation-drawer>
+  </nav>
+</template>
+
+<script>
+import Drawer from "./Drawer";
+
+export default {
+  name: "Navbar",
+  data() {
+    return {
+      showDrawer: false // Weather to show the navigation drawer or not
+    };
+  },
+  methods: {
+    /**
+     * Show or hide the drawer
+     */
+    toggleShowDrawer() {
+      this.showDrawer = !this.showDrawer;
+    }
+  },
+  components: {
+    Drawer // List of links
+  }
+};
+</script>
